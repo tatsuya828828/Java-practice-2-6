3b92b3fa65622e830ce6e35132d1476e8c4f07a9
tatsuya828828 <kkjy011894@gmail.com>
2020-07-04 23:10:06 +0900

    [start]Java-practice2-6

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static int twice(int x) {return x * 2;}
+
+	public static int sub(int a, int b) {return a - b;}
+
+	public static void main(String[] args) {
+		System.out.println(twice(5));
+		System.out.println(sub(3, 1));
+	}
+}
